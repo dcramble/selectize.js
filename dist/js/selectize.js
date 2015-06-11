d86a5d3e24ed39e9047d82bcee1eebf6ad9e0f24
dcramble <dcramble@mesd.k12.or.us>
2015-06-10 17:34:13 -0700

    update nowrap plugin for v0.12

--- dist/js/selectize.js
+++ dist/js/selectize.js
@@ -1212,7 +1212,7 @@
 			var events = silent ? [] : ['change'];
 	
 			debounce_events(this, events, function() {
-				this.clear();
+				this.clear(silent);
 				this.addItems(value, silent);
 			});
 		},
@@ -1797,6 +1797,22 @@
 		},
 	
 		/**
+		 * Returns an array of current options
+		 *
+		 * @returns {array}
+		 */
+		getOptions: function() {
+			var self = this;
+			var $options = [];
+	
+			for (var key in self.options) {
+				$options.push(self.options[key])
+			}
+	
+			return $options;
+		},
+	
+		/**
 		 * Returns the jQuery element of the next or
 		 * previous selectable option.
 		 *
@@ -1882,7 +1898,7 @@
 				}
 	
 				if (!self.options.hasOwnProperty(value)) return;
-				if (inputMode === 'single') self.clear();
+				if (inputMode === 'single') self.clear(silent);
 				if (inputMode === 'multi' && self.isFull()) return;
 	
 				$item = $(self.render('item', self.options[value]));
@@ -2176,7 +2192,7 @@
 			offset.top += $control.outerHeight(true);
 	
 			this.$dropdown.css({
-				width : $control.outerWidth(),
+				width : (typeof undefined !== typeof this.settings.dropdownOffsetWidth) ? ($control.outerWidth() + this.settings.dropdownOffsetWidth) : $control.outerWidth(),
 				top   : offset.top,
 				left  : offset.left
 			});
@@ -2640,8 +2656,6 @@
 		var field_optgroup_label = settings.optgroupLabelField;
 		var field_optgroup_value = settings.optgroupValueField;
 	
-		var optionsMap = {};
-	
 		/**
 		 * Initializes selectize from a <input type="text"> element.
 		 *
@@ -2681,6 +2695,7 @@
 		var init_select = function($input, settings_element) {
 			var i, n, tagName, $children, order = 0;
 			var options = settings_element.options;
+			var optionsMap = {};
 	
 			var readData = function($el) {
 				var data = attr_data && $el.attr(attr_data);
@@ -2876,6 +2891,56 @@
 			};
 		})();
 	
+	});
+	
+	/*global Selectize: true, measureString: true */
+	
+	Selectize.define('item_nowrap', function () {
+		var originalSetupTemplates = this.setupTemplates;
+		var paddingX;
+	
+		this.setupTemplates = function () {
+			// Call the original function, which sets the defaults.
+			originalSetupTemplates.apply(this, arguments);
+	
+			// Modify the rendering function for items to add an inner and outer
+			// wrapper element.
+			this.settings.render.item = function (data, escape) {
+				var field_label = this.settings.labelField;
+	
+				return '<div class="item">' +
+						  '<div class="item-wrap-outer">' +
+							'<div class="item-wrap-inner">' + escape(data[field_label]) + '</div>' +
+						  '</div>' +
+						'</div>';
+			};
+		};
+	
+		// When not active, the input is hidden off screen. This method adds
+		// some absolute positioning so that, in the event of a very long
+		// string of text, the rightmost portion of text is visible.
+		this.showInput = function () {
+			var $outer = this.$control.find('.item-wrap-outer'),
+				$inner = $outer.find('.item-wrap-inner'),
+				text = $inner.text(),
+				textWidth = measureString(text, $inner),
+				controlWidth = this.$control.width();
+	
+			paddingX = paddingX || (function (control) {
+				var paddingLeft = control.css('padding-left');
+				return parseInt(paddingLeft.match(/(\d+).*/)[1], 10);
+			}(this.$control));
+	
+			if (textWidth > controlWidth) {
+				$inner.addClass('long-input-value');
+				this.$control_input.css({ opacity: 1, position: 'absolute', left: $outer.width() + paddingX });
+			} else {
+				$inner.removeClass('long-input-value');
+				this.$control_input.css({ opacity: 1, position: 'absolute', left: textWidth + paddingX });
+			}
+	
+			this.isInputHidden = false;
+		};
 	});
 	
 	Selectize.define('optgroup_columns', function(options) {
